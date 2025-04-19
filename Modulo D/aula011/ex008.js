72ffc68f3b7fa847023dee129f10ede2148b18b1
CarlosLisboaa <cadubrsk1337@gmail.com>
2025-04-19 18:56:10 -0300

    a

---
+++ Modulo D/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocicidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
